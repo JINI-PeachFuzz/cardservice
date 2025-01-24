7f6f85d1d0fcc04bfc64de00220aec32f6550855
JINJU PARK <aricclunj@naver.com>
2025-01-24 15:18:27 +0900

    .

---
+++ cardservice/src/main/java/org/koreait/train/entities/TrainData.java
@@ -0,0 +1,4 @@
+package org.koreait.train.entities;
+
+public class TrainData {
+}
---
+++ cardservice/src/test/java/org/koreait/board/controller/BoardAdminControllerTest.java
@@ -0,0 +1,39 @@
+package org.koreait.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
+@AutoConfigureMockMvc
+public class BoardAdminControllerTest {
+
+    private
+
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    void init()
+    private RequestConfig();
+    form.setBid("freetalk");
+    form.setName("자유게시판")
+
+
+    @Test
+    @DisplayName("게시판 설정 등록 테스트")
+    void boardRegistertest() throws  Exception (
+            private
+
+            )
+
+
+om.readvalue ??? 
+}
+
+// 비회원일때문 비밀검증도 테스트하는거 추가해서 보기
